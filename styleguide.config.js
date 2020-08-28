e85366a0c5f5db84c4a1c8fb5b5e1b5f25cd11cb
Mahmood Bagheri <dr.cwna@gmail.com>
2020-08-28 20:26:49 +0430

    :wrench: Add styleguidist config file

---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    require: [path.join(__dirname, 'src/assets/styles/main.css')],
+    ignore: [
+        '**/__tests__/**',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/*.spec.{js,jsx,ts,tsx}',
+        '**/*.d.ts',
+        '**/styles.{js,jsx,ts,tsx}',
+        '**/index.{js,jsx,ts,tsx}',
+    ],
+};
