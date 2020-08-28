8e913748771f6313898be2345e1b7a1d8a13ace3
Mahmood Bagheri <dr.cwna@gmail.com>
2020-08-28 20:31:08 +0430

    :wrench: Locate components in styleguidist config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    require: [path.join(__dirname, 'src/assets/styles/main.css')],
+    require: [path.join(__dirname, "src/assets/styles/main.css")],
+    components: "src/components/**/[A-Z]*.tsx",
     ignore: [
-        '**/__tests__/**',
-        '**/*.test.{js,jsx,ts,tsx}',
-        '**/*.spec.{js,jsx,ts,tsx}',
-        '**/*.d.ts',
-        '**/styles.{js,jsx,ts,tsx}',
-        '**/index.{js,jsx,ts,tsx}',
+        "**/__tests__/**",
+        "**/*.test.{js,jsx,ts,tsx}",
+        "**/*.spec.{js,jsx,ts,tsx}",
+        "**/*.d.ts",
+        "**/styles.{js,jsx,ts,tsx}",
+        "**/index.{js,jsx,ts,tsx}",
     ],
 };
