afe5ad8c91386e921403e14d6a601e018d5c03fb
Mahmood Bagheri <dr.cwna@gmail.com>
2020-09-16 22:10:13 +0430

    🔧 Add npm check updates config file

---
+++ .ncurc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    reject: [
+        "@graphql-codegen/cli",
+        "@graphql-codegen/typescript",
+        "@graphql-codegen/typescript-operations",
+        "@graphql-codegen/typescript-react-apollo",
+    ],
+};
