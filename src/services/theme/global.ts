bbca14969af8a7c5b04e691f3a5c0ce98f7d4ccd
Mahmood Bagheri <dr.cwna@gmail.com>
2020-09-28 18:29:50 +0330

    ✨ Add dark theme

--- src/services/theme/global.ts
+++ src/services/theme/global.ts
@@ -1,5 +1,11 @@
-export const __THEME__ = {
+export const __LIGHT_THEME__ = {
     colors: {
         black: "#000",
+    },
+};
+
+export const __DARK_THEME__ = {
+    colors: {
+        black: "#fff",
     },
 };
