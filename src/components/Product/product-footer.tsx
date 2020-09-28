1144ce5faffd9c77b033082f74f8988abdaa07cc
Mahmood Bagheri <dr.cwna@gmail.com>
2020-09-28 18:09:44 +0330

    ✨ Add a sample product component

---
+++ src/components/Product/product-footer.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+
+/* styles */
+import { Div } from "./styles/product-footer.styles";
+
+type ProductFooterProps = {};
+
+export const ProductFooter: FunctionComponent<ProductFooterProps> = props => {
+    return <Div>product footer</Div>;
+};
